4b545fbdc8d8b95c568c54b414725f4523f0fde1
ProgrammStude <shawn.lacarta@noseryoung.ch>
2021-10-01 16:09:03 +0200

    implemented testing

---
+++ my-app/cypress/integration/testing/NikeSinglePage.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { mount } from "@cypress/react";
+import { BrowserRouter } from "react-router-dom";
+import NikeSinglePage from "../../../src/NikeSinglePage";
+
+describe("check search bar", () => {
+  it("shows result", () => {
+    // cy.pause();
+    cy.visit("http://localhost:3000/");
+    cy.contains("Nike").click();
+    cy.wait(2000);
+    cy.get(".form-control").type("90").should("have.value", "90");
+    cy.wait(2000);
+    cy.contains("Jordan").click();
+    cy.wait(2000);
+    cy.get(".form-control").type("11").should("have.value", "11");
+  
+  });
+});
